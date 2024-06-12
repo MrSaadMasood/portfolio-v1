cf18a8bec19d9c57bbbfa8b50854949f87fb9839
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-06-12 13:45:48 +0500

    chore: remove console

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -3,7 +3,5 @@ import { dbConnect } from "@/lib/connection"
 export async function GET() {
   const db = await dbConnect()
   const projects = await db.collection("data").find().sort({ time: -1 }).toArray()
-  console.log(projects);
-
   return Response.json(projects)
 }
