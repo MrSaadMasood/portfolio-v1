4e0aaa8768fb4e5f1a4aa8a9fbec9115e4b54b6b
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-06-13 18:43:25 +0500

    fixed top position of the sidebar navigation for small screens

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -4,7 +4,7 @@ import { v4 as uuid } from "uuid"
 export default function SideBar() {
   const navigationLinks = ["Home", "Project", "Contact"]
   return (
-    <header className="w-[30%] h-[20%] font-bold absolute top-36 md:top-44 text-xs">
+    <header className="w-[30%] h-[20%] font-bold absolute top-44 md:top-44 text-xs">
       <ol className="">
         {navigationLinks.map((link) => (
           <Navigation key={uuid()} link={link} />
