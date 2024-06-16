81a970f9fbd69c0f16a816345fe0dcc1a5e44e37
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-06-16 14:21:42 +0500

    feat: add page and global error boundary

---
+++ src/app/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+import useMonoSpaced from "@/hooks/useMonoSpaced";
+import { clsx } from "clsx"
+
+export default function Error({ error, reset }: {
+  error: Error,
+  reset: () => void
+}) {
+  const { isMonoSpaced } = useMonoSpaced()
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center ">
+      <h2 className={clsx(`text-4xl md:text-5xl text-[#d0d0d0] hover:opacity-65
+        duration-300 dark:text-black font-extrabold mb-7 text-center`,
+        isMonoSpaced ?
+          "neue-mono tracking-tighter" : "neue-reg"
+      )}>
+        {error.message || "Oops! Something Went Wrong!"}</h2>
+      <button className="p-3 text-black font-bold bg-white rounded-lg
+          hover:cursor-pointer dark:bg-black dark:text-white hover:bg-[#949494] "
+        onClick={() => reset()}>Try Again!</button>
+    </div >
+  );
+}
